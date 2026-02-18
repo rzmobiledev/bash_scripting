d6eed770b3b1faf93b9b836cf323efd40c880658
rizal <rzmobiledev@gmail.com>
2026-02-18 19:20:04 +0700

    save arithmetic notes

---
+++ arithmetic-expression.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# a=2
+# b=3
+#
+# ((max = a > b ? a : b))
+#
+# echo "max = $max"
+
+# example 01
+# a=10
+#
+# if ((a % 2)); then
+#   echo number is odd
+# else
+#   echo number is even
+# fi
+
+#example 02
+# i = 0
+# while ((i < 10)); do
+#   echo "i is $i"
+#   ((i++))
+# done
+
+# example 03
+# a=010
+# echo "$a"
+# echo $((a))
